68e2d13d2faecedea3f266d0a3423e4b1f09e5f3
unknown <lucusapp@gmail.com>
2019-12-06 20:02:12 +0100

    06122019

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AngularComponent } from './pages/angular/angular.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'portafolio', component: PortafolioComponent},
+{path:'angular', component: AngularComponent},
+{path:'**', pathMatch:'full',redirectTo:'portafolio'},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { AngularComponent } from './pages/angular/angular.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PortafolioComponent
+    PortafolioComponent,
+    AboutComponent,
+    AngularComponent
   ],
   imports: [
     BrowserModule,
